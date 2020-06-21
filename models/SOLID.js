769bec7c4124175d7d92d191219ede3cd263a02b
kbjude <kbjude@gmail.com>
2020-06-21 14:48:46 +0300

    complete SOLID

--- models/SOLID.js
+++ models/SOLID.js
@@ -211,4 +211,85 @@ class DOMTraverser {
             // ...
         }
     }
+
+    traverse() {
+        // ...
+    }
 }
+
+const $ = new DOMTraverser({
+    rootNode: document.getElementsByTagName('body'),
+    options: {
+        animationModule() {}
+    }
+});
+
+// Dependency inversion
+//BAD
+
+class InventoryRequester {
+    constructor() {
+        this.REQ_METHODS = ['HTTP'];
+    }
+
+    requestItem(item) {
+        //....
+    }
+}
+
+class InventoryTracker {
+    constructor(items) {
+        this.items = items;
+
+        //BAD: We have create a dependency on a specific request implementation. 
+        // We should just have requestitems depend on a request method: 'request'
+        this.requester = new InventoryRequester();
+    }
+
+    requestItems() { 
+        this.items.forEach((item) => {
+            this,requester.requestItem(item);
+        });
+    }
+}
+    const inventoryTrucker = new InventoryTracker(['apples', 'bananas']);
+    inventoryTracker.requestItems();
+
+    // GOOD 
+
+class InventoryTracker {
+    constructor(items, requester) {
+            this.items = items;
+            this.requester = requester;
+    }
+    requestItems() {
+        this.items.forEach((item) => {
+            this.requester.requestItem(item);
+        });
+    }
+}
+
+class InventoryRequesterV1 {
+    constructor() {
+        this.REQ_METHODS = ['HTTP'];
+    }
+
+    requestItems(item) {
+        // ...
+    }
+}
+
+class InventoryRequesterV2 {
+    constructor() {
+        this.REQ_METHODS = ['WS'];
+    }
+
+    requestItems(item) {
+        // ...
+    }
+}
+
+// By constructing our dependencies externally and injecting them, we can easily
+//substitute our request module for a fancy new one that uses websockets.
+const inventoryTracker = new InventoryTracker(['apples', 'bananas'], new InventoryRequesterV2());
+inventoryTracker.requestItems();
